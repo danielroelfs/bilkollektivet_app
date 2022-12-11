b1e2528de9c9de04dacea1bb114716201aaf1299
Daniel Roelfs <daniel@roelfs.net>
2022-12-11 15:03:09 +0100

    update to match minor bilkollektivet html changes

--- server.R
+++ server.R
@@ -42,23 +42,28 @@ shinyServer(function(input, output) {
     html_nodes("div.fl-col") |>  
     html_text() |> 
     str_remove_all("\\n|\\t") |> 
-    {\(.) .[seq(18, 29)]}() |> 
+    {\(.) .[seq(19, 31)]}() |> 
     as_tibble() |> 
-    separate(col = value, 
-             into = c("cartype", "startgebyr", "price_hour", "price_day", "price_week", "price_km"),
-             sep = "[[:space:]]{12}") |> 
+    separate(col = value,
+             into = c("cartype", "prices"),
+             sep = "Start") |> 
+    mutate(cartype = str_trim(cartype)) |> 
+    filter(nchar(cartype) > 0) |>
+    separate(col = cartype, 
+             into = c("carname", "category"), 
+             sep ="(?<=[a-z|0-9|L|Y])(?=[A-Z|9])") |> 
+    separate(col = prices, 
+             into = c(NA, "price_start", "price_hour", "price_day", "price_week", "price_km"),
+             sep = ":") |> 
     mutate(
       across(everything(), str_trim),
       price_week = str_remove_all(price_week, "1 uke:"),
-      across(startgebyr:last_col(), 
+      across(price_start:last_col(), 
              ~ parse_number(.x, locale = locale(decimal_mark = ",", 
-                                                grouping_mark = " ")))
-    ) |> 
-    separate(col = cartype, into = c("carname", "category"), 
-             sep ="(?<=[a-z|0-9|L|Y])(?=[A-Z|9])") |> 
-    mutate(car = ifelse(str_detect(category, "Budsjett"), 
-                        yes = str_glue("{carname} ({category})"), 
-                        no = carname))
+                                                grouping_mark = " "))),
+      car = ifelse(str_detect(category, "Budsjett"), 
+                   yes = str_glue("{carname} ({category})"), 
+                   no = carname))
   
   # Show cartype as title
   output$title <- renderText({
