4eb07767ff400e5de377c30750d76dcb7592f5db
Daniel Roelfs <daniel@roelfs.net>
2024-01-30 20:16:20 +0100

    minor updates and linting

--- server.R
+++ server.R
@@ -10,69 +10,81 @@ library(gt)
 #-- Define server -------------------------
 
 shinyServer(function(input, output) {
-  
   # Get variables
   dist <- reactive({
     dist <- input$dist
   })
-  
+
   n_days <- reactive({
     n_days <- input$n_days
   })
-  
+
   n_hours <- reactive({
     n_hours <- input$n_hours
   })
-  
+
   n_pass <- reactive({
     n_pass <- input$n_pass
   })
-  
+
   car <- reactive({
     car <- input$car
   })
-  
+
   insurance <- reactive({
     insurance <- input$insurance
   })
-  
+
   # Input base prices
-  pricelist <- "https://bilkollektivet.no/priser/" |> 
-    read_html() |> 
-    html_nodes("div.fl-col") |>  
-    html_text() |> 
-    str_remove_all("\\n|\\t") |> 
-    {\(.) .[seq(19, 31)]}() |> 
-    as_tibble() |> 
-    separate(col = value,
-             into = c("cartype", "prices"),
-             sep = "Start") |> 
-    mutate(cartype = str_trim(cartype)) |> 
+  pricelist <- "https://bilkollektivet.no/priser/" |>
+    read_html() |>
+    html_nodes("div.fl-col") |>
+    html_text() |>
+    str_remove_all("\\n|\\t") |>
+    {
+      \(.) .[seq(19, 31)]
+    }() |>
+    as_tibble() |>
+    separate(
+      col = value,
+      into = c("cartype", "prices"),
+      sep = "Start"
+    ) |>
+    mutate(cartype = str_trim(cartype)) |>
     filter(nchar(cartype) > 0) |>
-    separate(col = cartype, 
-             into = c("carname", "category"), 
-             sep ="(?<=[a-z|0-9|L|Y])(?=[A-Z|9])") |> 
-    separate(col = prices, 
-             into = c(NA, "price_start", "price_hour", "price_day", "price_week", "price_km"),
-             sep = ":") |> 
+    separate(
+      col = cartype,
+      into = c("carname", "category"),
+      sep = "(?<=[a-z|0-9|L|Y])(?=[A-Z|9])"
+    ) |>
+    separate(
+      col = prices,
+      into = c(NA, "price_start", "price_hour", "price_day", "price_week", "price_km"),
+      sep = ":"
+    ) |>
     mutate(
       across(everything(), str_trim),
       price_week = str_remove_all(price_week, "1 uke:"),
-      across(price_start:last_col(), 
-             ~ parse_number(.x, locale = locale(decimal_mark = ",", 
-                                                grouping_mark = " "))),
-      car = ifelse(str_detect(category, "Budsjett"), 
-                   yes = str_glue("{carname} ({category})"), 
-                   no = carname))
-  
+      across(
+        price_start:last_col(),
+        ~ parse_number(.x, locale = locale(
+          decimal_mark = ",",
+          grouping_mark = " "
+        ))
+      ),
+      car = ifelse(str_detect(category, "Budsjett"),
+        yes = str_glue("{carname} ({category})"),
+        no = carname
+      )
+    )
+
   # Show cartype as title
   output$title <- renderText({
     car()
   })
-  
+
   # Show base prices
   output$baseprices <- render_gt({
-    
     carlabels <- tribble(
       ~car, ~linkname,
       "Toyota Yaris (Budsjettklasse)", "https://bilkollektivet.no/content/uploads/2022/08/Yaris_600x250.png",
@@ -88,19 +100,20 @@ shinyServer(function(input, output) {
       "Tesla Model Y", "https://bilkollektivet.no/content/uploads/2021/08/TeslaY-250x600-1.png",
       "Toyota Rav4", "https://bilkollektivet.no/content/uploads/2019/09/Toyota-Rav4_250x600.png",
     )
-    
-    url <- carlabels |> 
-      filter(car == car()) |> 
+
+    url <- carlabels |>
+      filter(car == car()) |>
       pull(linkname)
-    
-    pricelist |> 
-      select(car, category, starts_with("price")) |> 
-      filter(car == car()) |> 
-      gt() |> 
+
+    pricelist |>
+      select(-price_start) |>
+      select(car, category, starts_with("price")) |>
+      filter(car == car()) |>
+      gt() |>
       tab_options(
         table.width = pct(80),
         column_labels.font.weight = "bold",
-      ) |> 
+      ) |>
       tab_style(
         style = list(
           cell_borders(
@@ -113,7 +126,7 @@ shinyServer(function(input, output) {
             columns = everything()
           )
         )
-      ) |> 
+      ) |>
       cols_label(
         car = "",
         category = "Class",
@@ -121,7 +134,7 @@ shinyServer(function(input, output) {
         price_day = "Døgnpris",
         price_week = "Ukepris",
         price_km = "Per kilometer"
-      ) |> 
+      ) |>
       text_transform(
         locations = cells_body(car),
         fn = function(x) {
@@ -130,15 +143,23 @@ shinyServer(function(input, output) {
             height = 80
           )
         }
-      ) |> 
+      ) |>
       fmt_currency(
-        columns = starts_with("price"), 
+        columns = starts_with("price"),
         currency = "NOK",
         sep_mark = " ",
         dec_mark = ",",
-        use_subunits = TRUE, 
+        use_subunits = TRUE,
+        incl_space = TRUE,
+      ) |>
+      fmt_currency(
+        columns = c("price_hour", "price_day", "price_week"),
+        currency = "NOK",
+        sep_mark = " ",
+        pattern = "{x},-",
+        use_subunits = FALSE,
         incl_space = TRUE
-      ) |> 
+      ) |>
       tab_style(
         style = list(
           cell_borders(
@@ -154,20 +175,20 @@ shinyServer(function(input, output) {
         )
       )
   })
-  
+
   # Calculate breakdown
   output$breakdown <- render_gt({
-    prices <- pricelist |> 
+    prices <- pricelist |>
       filter(car == car())
-    
-    # Calculate kilometer price 
+
+    # Calculate kilometer price
     dist_km <- dist()
     price_km <- prices$price_km * dist_km
-    
+
     # Calculate time price
     days_price <- prices$price_day * n_days()
     hours_price <- prices$price_hour * n_hours()
-    
+
     if (n_days() < 7) {
       discount <- 0
       discount_day <- 0
@@ -178,7 +199,7 @@ shinyServer(function(input, output) {
       discount <- 0.30
       discount_day <- prices$price_day * discount * -1
     }
-    
+
     if (input$insurance) {
       ins_cost <- (n_hours() * 14) + (n_days() * 96)
       if (n_days() >= 1) {
@@ -193,10 +214,10 @@ shinyServer(function(input, output) {
       ins_unit <- 0
       ins_cost_base <- 0
     }
-    
+
     discount_text <- pct(discount * 100)
     discount_price <- ifelse(discount > 0, days_price * discount * -1, 0)
-    
+
     # Create breakdown table
     breakdown_table <- tribble(
       ~Item, ~Units, ~Baseprice, ~Price,
@@ -206,67 +227,68 @@ shinyServer(function(input, output) {
       "Distance", dist_km, prices$price_km, price_km,
       "Additional insurance", ins_unit, ins_cost_base, ins_cost
     )
-    
-    breakdown_table |> 
-      gt(rowname_col = "Item") |> 
+
+    breakdown_table |>
+      gt(rowname_col = "Item") |>
       tab_options(
         table.width = pct(80),
-        grand_summary_row.background.color = "#8A29BE", 
+        grand_summary_row.background.color = "#8A29BE",
         column_labels.font.weight = "bold"
-      ) |> 
+      ) |>
       fmt(
         columns = c(Units),
         rows = contains("Discount"),
         fns = function(x) pct(x * 100)
-      ) |> 
+      ) |>
       fmt_currency(
-        columns = c(Baseprice), 
+        columns = c(Baseprice),
         currency = "NOK",
         sep_mark = " ",
         dec_mark = ",",
         incl_space = TRUE
-      ) |> 
+      ) |>
       fmt_currency(
-        columns = c(Price), 
-        currency = "NOK", 
+        columns = c(Price),
+        currency = "NOK",
         use_subunits = FALSE,
         sep_mark = " ",
         dec_mark = ",",
-        pattern = "{x},-",
-        incl_space = TRUE
-      ) |> 
+        incl_space = TRUE,
+        pattern = "{x},-"
+      ) |>
       tab_header(
         title = md("**How much will my trip cost?**")
-      ) |> 
+      ) |>
       grand_summary_rows(
         columns = c(Price),
-        fns = list(Total = ~sum(., na.rm = TRUE)),
-        formatter = fmt_currency,
-        currency = "NOK", 
-        use_subunits = FALSE,
-        sep_mark = " ",
-        dec_mark = ",",
-        pattern = "{x},-",
-        incl_space = TRUE
-      ) |> 
+        fns = list(Total = ~ sum(., na.rm = TRUE)),
+        fmt = ~ fmt_currency(
+          .,
+          currency = "NOK",
+          use_subunits = FALSE,
+          sep_mark = " ",
+          dec_mark = ",",
+          incl_space = TRUE
+        )
+      ) |>
       tab_footnote(
-        footnote =  md("Discount of 20% on rental periods of 7 days or longer, 30% for 14 days or longer"),
+        footnote = md("Discount of 20% on rental periods of 7 days or longer, 30% for 14 days or longer"),
         locations = cells_body(columns = "Units", rows = contains("Discount"))
-      ) |> 
+      ) |>
       tab_footnote(
-        footnote =  md("Additional insurance is 14 NOK per hour or 96 NOK per day"),
+        footnote = md("Additional insurance is 14 NOK per hour or 96 NOK per day"),
         locations = cells_body(columns = "Units", rows = contains("Additional insurance"))
-      ) |> 
+      ) |>
       tab_footnote(
-        footnote =  md("Fuel and tolls included"),
+        footnote = md("Tolls included, fuel included only for non-electric vehicles"),
         locations = cells_grand_summary(
           columns = c(Price)
         )
-      ) |> 
+      ) |>
       cols_align(
         align = "center",
         columns = c(Units)
-      ) |> 
+      ) |>
       tab_style(
         style = list(
           cell_borders(
@@ -280,7 +302,7 @@ shinyServer(function(input, output) {
             columns = everything()
           )
         )
-      ) |> 
+      ) |>
       tab_style(
         style = list(
           cell_borders(
@@ -296,28 +318,27 @@ shinyServer(function(input, output) {
         )
       )
   })
-  
+
   # Calculate individual price
   output$per_passenger <- render_gt({
-    
-    prices <- pricelist |> 
+    prices <- pricelist |>
       filter(car == car())
-    
-    # Calculate kilometer price 
+
+    # Calculate kilometer price
     dist_km <- dist()
     price_km <- prices$price_km * dist()
-    
+
     # Calculate time price
     days_price <- prices$price_day * n_days()
     hours_price <- prices$price_hour * n_hours()
-    
+
     # Calculate insurance price
     if (input$insurance) {
       ins_cost <- (n_hours() * 14) + (n_days() * 96)
     } else {
       ins_cost <- 0
     }
-    
+
     if (n_days() < 7) {
       discount <- 0
     } else if (n_days() >= 7 & n_days() < 14) {
@@ -325,36 +346,35 @@ shinyServer(function(input, output) {
     } else if (n_days() >= 14) {
       discount <- 0.30
     }
-    
+
     discount_price <- ifelse(discount > 0, days_price * discount * -1, 0)
-    
+
     total_price <- sum(days_price, discount_price, hours_price, price_km, ins_cost)
     passenger_price <- total_price / n_pass()
-    
+
     if (n_pass() == 1) {
-      
       tribble(
         ~text, ~Price,
         "Total price is equal to individual price", total_price,
-      ) |> 
-        gt() |> 
-        cols_label(text = "") |> 
+      ) |>
+        gt() |>
+        cols_label(text = "") |>
         tab_options(
           table.width = pct(50),
           column_labels.font.weight = "bold"
-        ) |> 
+        ) |>
         fmt_currency(
-          columns = c(Price), 
-          currency = "NOK", 
+          columns = c(Price),
+          currency = "NOK",
           use_subunits = FALSE,
           sep_mark = " ",
           dec_mark = ",",
-          pattern = "{x},-",
+          pattern = "{x}",
           incl_space = TRUE
-        ) |> 
+        ) |>
         tab_header(
           title = md("**Not splitting the costs**")
-        ) |> 
+        ) |>
         tab_style(
           style = list(
             cell_borders(
@@ -369,32 +389,30 @@ shinyServer(function(input, output) {
             )
           )
         )
-      
     } else {
-      
       tribble(
         ~text, ~Price,
         "Total price for trip", total_price,
         "Price per passenger", passenger_price,
-      ) |> 
-        gt() |> 
-        cols_label(text = "") |> 
+      ) |>
+        gt() |>
+        cols_label(text = "") |>
         tab_options(
           table.width = pct(50),
           column_labels.font.weight = "bold"
-        ) |> 
+        ) |>
         fmt_currency(
-          columns = c(Price), 
-          currency = "NOK", 
+          columns = c(Price),
+          currency = "NOK",
           use_subunits = FALSE,
           sep_mark = ".",
           dec_mark = ",",
-          pattern = "{x},-",
+          pattern = "{x}",
           incl_space = TRUE
-        ) |> 
+        ) |>
         tab_header(
           title = md(sprintf("**Splitting the cost between %s people**", n_pass()))
-        ) |> 
+        ) |>
         tab_style(
           style = list(
             cell_borders(
@@ -411,5 +429,4 @@ shinyServer(function(input, output) {
         )
     } # end of if-else
   })
-  
 })
--- ui.R
+++ ui.R
@@ -10,102 +10,116 @@ library(gt)
 #-- Define UI -------------------------
 
 shinyUI(fluidPage(
-    theme = "style.css",
-    
-    # Application title
-    titlePanel(title = "Bilkollektivet trip cost", 
-               windowTitle = "Cost Bilkollektivet"),
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            tags$style(".well {background-color: transparent;border: none; box-shadow: none}"),
-            chooseSliderSkin("Flat", color = "#8A29BE"),
-            sliderInput(inputId = "dist",
-                        label = "Estimated distance:",
-                        min = 10,
-                        max = 2000,
-                        value = 250, 
-                        step = 10,
-                        ticks = FALSE),
-            sliderInput(inputId = "n_days",
-                        label = "Number of days:",
-                        min = 0,
-                        max = 14,
-                        value = 2,
-                        ticks = FALSE),
-            sliderInput(inputId = "n_hours",
-                        label = "Number of hours:",
-                        min = 0,
-                        max = 7,
-                        value = 0,
-                        ticks = FALSE),
-            sliderInput(inputId = "n_pass",
-                        label = "Number of passengers (incl. driver):",
-                        min = 1,
-                        max = 9,
-                        value = 1,
-                        ticks = FALSE),
-            radioGroupButtons(inputId = "car",
-                              label = "Select car type:", 
-                              #choices = c("Liten elbil", #= elbil_liten
-                              #            "Småbil", #= "smabil",
-                              #            "Mellomklasse elbil", #= "elbil_mellom",
-                              #            "Stasjonsvogn", #= "stasjonsvogn",
-                              #            "Sportsbil", #= "sportsbil",
-                              #            "Tesla Model 3", #= "elbil",
-                              #            "Tesla Model Y", #= "elbil_premium",
-                              #            #"Jaguar Ipace", #= "elbil_premium",
-                              #            #"7-seter", #= "7-seter",
-                              #            "SUV 4x4", #= "suv",
-                              #            "El-varebil", #= "varebil_el",
-                              #            "9-seter", #= "9-seter",
-                              #            "Varebil" #= "varebil",
-                              #),
-                              choices = c("Toyota Yaris (Budsjettklasse)",
-                                          "Opel Corsa-e",
-                                          "Toyota Yaris",
-                                          "Toyota Yaris Cross",
-                                          "Toyota Corolla",
-                                          "Toyota Proace EL",
-                                          "Toyota Proace",
-                                          "Toyota Proace Verso",
-                                          "Mazda MX5",
-                                          "Tesla Model 3",
-                                          "Tesla Model Y",
-                                          "Toyota Rav4"
-                                          ),
-                              checkIcon = list(yes = icon("check")), 
-                              justified = FALSE,
-                              width = "300px",
-                              #selected = "Mellomklasse elbil", 
-                              #choicesOpt = list(
-                              #    content = sprintf("<span class='label label-%s'>%s</span>", 
-                              #                      c("success", "success",
-                              #                        "success", "success", 
-                              #                        "primary","primary","primary",
-                              #                        "primary", "primary", "primary",
-                              #                        "info","info"), 
-                              #                      c("Mellomklasse elbil", "Premium elbil",
-                              #                        "Elektrisk lastesykkel","Elektrisk varebil",
-                              #                        "Småbil", "Mellomklasse", "Stasjonsvogn",
-                              #                        "7-seter", "SUV 4x4", "9-seter", 
-                              #                        "Varebil", "Stor varebil")))
-            ),
-            checkboxInput(inputId = "insurance",
-                          label = "Additional insurance",
-                          value = FALSE)
+  theme = "style.css",
+
+  # Application title
+  titlePanel(
+    title = "Bilkollektivet trip cost calculator",
+    windowTitle = "Bilkollektivet Calculator"
+  ),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      tags$style(".well {background-color: transparent;border: none; box-shadow: none}"),
+      chooseSliderSkin("Flat", color = "#8A29BE"),
+      sliderInput(
+        inputId = "dist",
+        label = "Estimated distance:",
+        min = 10,
+        max = 2000,
+        value = 250,
+        step = 10,
+        ticks = FALSE
+      ),
+      sliderInput(
+        inputId = "n_days",
+        label = "Number of days:",
+        min = 0,
+        max = 14,
+        value = 2,
+        ticks = FALSE
+      ),
+      sliderInput(
+        inputId = "n_hours",
+        label = "Number of hours:",
+        min = 0,
+        max = 7,
+        value = 0,
+        ticks = FALSE
+      ),
+      sliderInput(
+        inputId = "n_pass",
+        label = "Number of passengers (incl. driver):",
+        min = 1,
+        max = 9,
+        value = 1,
+        ticks = FALSE
+      ),
+      radioGroupButtons(
+        inputId = "car",
+        label = "Select car type:",
+        # choices = c("Liten elbil", #= elbil_liten
+        #            "Småbil", #= "smabil",
+        #            "Mellomklasse elbil", #= "elbil_mellom",
+        #            "Stasjonsvogn", #= "stasjonsvogn",
+        #            "Sportsbil", #= "sportsbil",
+        #            "Tesla Model 3", #= "elbil",
+        #            "Tesla Model Y", #= "elbil_premium",
+        #            #"Jaguar Ipace", #= "elbil_premium",
+        #            #"7-seter", #= "7-seter",
+        #            "SUV 4x4", #= "suv",
+        #            "El-varebil", #= "varebil_el",
+        #            "9-seter", #= "9-seter",
+        #            "Varebil" #= "varebil",
+        # ),
+        choices = c(
+          "Toyota Yaris (Budsjettklasse)",
+          "Opel Corsa-e",
+          "Toyota Yaris",
+          "Toyota Yaris Cross",
+          "Toyota Corolla",
+          "Toyota Proace EL",
+          "Toyota Proace",
+          "Toyota Proace Verso",
+          "Mazda MX5",
+          "Tesla Model 3",
+          "Tesla Model Y",
+          "Toyota Rav4"
         ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            span(h1(textOutput("title")), style = "text-indent: 10%"),
-            gt_output("baseprices"),
-            br(),
-            gt_output("breakdown"),
-            br(),
-            gt_output("per_passenger"),
-            br()
-        )
+        checkIcon = list(yes = icon("check")),
+        justified = FALSE,
+        width = "300px",
+        # selected = "Mellomklasse elbil",
+        # choicesOpt = list(
+        #    content = sprintf("<span class='label label-%s'>%s</span>",
+        #                      c("success", "success",
+        #                        "success", "success",
+        #                        "primary","primary","primary",
+        #                        "primary", "primary", "primary",
+        #                        "info","info"),
+        #                      c("Mellomklasse elbil", "Premium elbil",
+        #                        "Elektrisk lastesykkel","Elektrisk varebil",
+        #                        "Småbil", "Mellomklasse", "Stasjonsvogn",
+        #                        "7-seter", "SUV 4x4", "9-seter",
+        #                        "Varebil", "Stor varebil")))
+      ),
+      checkboxInput(
+        inputId = "insurance",
+        label = "Additional insurance",
+        value = FALSE
+      )
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      span(h1(textOutput("title")), style = "text-indent: 10%"),
+      gt_output("baseprices"),
+      br(),
+      gt_output("breakdown"),
+      br(),
+      gt_output("per_passenger"),
+      br()
     )
+  )
 ))
